b36301f605e9ed665273471b974683e97d5e7d66
olarno <arnaudoltra@hotmail.fr>
2022-05-06 18:28:43 +0200

    add ERC721 protocol
    Add transaction on ERC 721 protocol
    Very basic

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -25,7 +25,7 @@ contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
     // 1. Create modifier here
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         _;
     }
@@ -52,7 +52,7 @@ contract ZombieFeeding is ZombieFactory {
         uint _zombieId,
         uint _targetDna,
         string memory _species
-    ) internal ownerOf(_zombieId) {
+    ) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
